5197149bbc6439b25b8366bfa175ef39c9c9ecc1
korean-god <124107282+korean-god@users.noreply.github.com>
2023-02-06 12:54:43 +0900

    First and Second Project
    Understanding Widgets : AppBar, Colors, Drawer, leading, Column, Center

---
+++ first_project/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My First App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello!'),
+            Text('Hello!'),
+            Text('Hello!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
