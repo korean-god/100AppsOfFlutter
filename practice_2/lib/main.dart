ac5e738ac95a590ef87e6c5607ea6d74fd280e64
korean-god <124107282+korean-god@users.noreply.github.com>
2023-02-09 18:56:56 +0900

    multi navigator practice

--- practice_2/lib/main.dart
+++ practice_2/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:practice_2/homepage.dart';
+import 'package:practice_2/nextpage.dart';
 
-void main() {
-  runApp(const MainApp());
+void main(List<String> args) {
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/a': (context) => NextPage(),
+      },
     );
   }
 }
