71d54a8817506964aec7e2c7311c701204b3fd9f
korean-god <124107282+korean-god@users.noreply.github.com>
2023-02-11 16:50:49 +0900

    element tree understadin

---
+++ stl_example_to_understand_render_tree/README.md
@@ -0,0 +1,3 @@
+# stl_example_to_understand_render_tree
+
+A new Flutter project.
---
+++ stl_example_to_understand_render_tree/android/app/src/main/kotlin/com/example/stl_example_to_understand_render_tree/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stl_example_to_understand_render_tree
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
